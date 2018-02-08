ab4a71df4fea93f341e7f35c223ff77edd9e768c
Htt <2010htt@sina.com>
2018-02-08 09:10:34 +0000

    user index

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -4,11 +4,16 @@
     <title>@yield('title','Sample')</title>
   </head>
   <body>
+    <form action="{{ route('logout') }}" method="POST">
+  {{ csrf_field() }}
+  {{ method_field('DELETE') }}
+  <button class="btn btn-block btn-danger" type="submit" name="button">退出</button>
+</form>
     <div class="container">
       <div class="col-md-offset-1 col-md-10">
         @include('shared._messages')
         @yield('content')
-        
+
       </div>
     </div>
 
